210ed7c5e2460e0ab2e4cf00f2e455a5f10d8756
BmudGit <168735110+BmudGit@users.noreply.github.com>
2025-01-14 12:48:25 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = bmudterraformremote
+    bucket         = "bmudterraformremote"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
